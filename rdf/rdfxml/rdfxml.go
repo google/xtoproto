0476e9fb6df1a5c82c3f2b3fde70a8b71e165004
Red Daly <reddaly@google.com>
2020-12-05 03:42:30 +0000

    Fix README, add empty rdfxml package, add rdf tests.

---
+++ rdf/rdfxml/rdfxml.go
@@ -0,0 +1,37 @@
+package rdfxml
+
+import (
+	"encoding/xml"
+
+	"github.com/google/xtoproto/rdf/ntriples"
+)
+
+const (
+	// RDF if the base IRI for RDF terms.
+	RDF ntriples.IRI = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+)
+
+// IRIs for terms defined in https://www.w3.org/TR/rdf-schema/.
+const (
+	RDFLangString  ntriples.IRI = RDF + "langString"
+	RDFHTML        ntriples.IRI = RDF + "HTML"
+	RDFXMLLiteral  ntriples.IRI = RDF + "XMLLiteral"
+	RDFXMLProperty ntriples.IRI = RDF + "XMLProperty"
+	RDFType        ntriples.IRI = RDF + "type"
+	RDFProperty    ntriples.IRI = RDF + "Property"
+)
+
+// IterationDecision indicates whether an iterating function should stop
+// iterating.
+type IterationDecision int
+
+// Valid IterationDecision values.
+const (
+	Stop IterationDecision = iota
+	Continue
+)
+
+// StreamTriples decodes triples from an XML token stream.
+func ReadTriples(reader xml.TokenReader, receiver func(t *ntriples.Triple) (IterationDecision, error)) {
+
+}
---
+++ rdf/rdfxml/rdfxml_test.go
@@ -0,0 +1,11 @@
+package rdfxml
+
+import (
+	"testing"
+)
+
+// Test cases based on the {Positive, Negative} Parser Tests in
+// https://www.w3.org/TR/rdf-testcases/.
+
+func TestReadTriples_positive(t *testing.T) {
+}
